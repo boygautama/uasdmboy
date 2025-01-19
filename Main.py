494c17c844ced952a1aa8d73e15bc31192e024fb
Boy Gautama <67629996+boygautama@users.noreply.github.com>
2025-01-19 13:00:20 +0000

    .

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import io
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
